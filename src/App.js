a1180d06ca4a72380e59061a4256e0eeddc38739
unknown <bhmyacine@gmail.com>
2022-01-02 23:35:36 +0100

    update my work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from "react";
-import "./App.css";
-import Nav from "./component/navbar.js";
-import MovieList from "./component/MovieList.jsx";
+import './App.css';
+import {  Button, Modal } from 'react-bootstrap/';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {  useState } from "react";
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className="App">
-      <Nav />
-      <MovieList />
-    </div>
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+         
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
+
 export default App;
+
---
+++ src/AppMovies.js
@@ -0,0 +1,130 @@
+import React, { useEffect, useState } from "react";
+import './MovieCard.css';
+import { Form, Col, Button, Navbar, Nav, FormControl, Modal} from 'react-bootstrap/';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MovieList from "./MovieList";
+import MovieAdd from "./MovieAdd"
+import Filtre from "./Filtre"
+
+
+
+const AppMovies = () => {
+
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const [movies, setMovies] = useState([
+        {
+            titre: "Terminator",
+            description: "An augmented human and Sarah Connor must stop an advanced liquid Terminator from hunting down a young girl, whose fate is critical to the human race. whose fate is critical to the human race whose fate is critical to the human race ",
+            url: "terminator.jpg",
+            note: '5',
+            imgSrc:'terminator.jpg'},
+        {
+            titre: "Assassin's Creed",
+            description: "In the film, Callum Lynch explores the memories of his ancestor Aguilar de Nerha and gains the skills of a Master Assassin, before taking on the secret Templar society. Filming began in late August 2015 and concluded in January 2016.",
+            url: "123456",
+            note: '5',
+            imgSrc:"assasinscreed.jpg"
+        },
+        {
+            titre: "Last Samurai",
+            description: "In the film, Callum Lynch explores the memories of his ancestor Aguilar de Nerha and gains the skills of a Master Assassin, before taking on the secret Templar society. Filming began in late August 2015 and concluded in January 2016.",
+            url: "123456",
+            note: '5',
+            imgSrc:"download.jpg"
+        },
+        {
+            titre: "Mission Impossble",
+            description: "In the film, Callum Lynch explores the memories of his ancestor Aguilar de Nerha and gains the skills of a Master Assassin, before taking on the secret Templar society. Filming began in late August 2015 and concluded in January 2016.",
+            url: "123456",
+            note: '5',
+            imgSrc:"mission.jpg"
+        },
+        {
+            titre: "Need For Speed",
+            description: "In the film, Callum Lynch explores the memories of his ancestor Aguilar de Nerha and gains the skills of a Master Assassin, before taking on the secret Templar society. Filming began in late August 2015 and concluded in January 2016.",
+            url: "123456",
+            note: '5',
+            imgSrc:"need.jpg"
+        },
+        {
+            titre: "The Mummy",
+            description: "An ancient Egyptian princess is awakened from her crypt beneath the desert, bringing with her malevolence grown over millennia, and terrors that defy human comprehension.",
+            url: "123456",
+            note: '5',
+            imgSrc:"the_mummy.jpg"
+        },
+    ]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+    const handleMovieAdd = (newValue) => {
+        setMovies([...movies, newValue]);
+    };
+    
+    const handleChange = (searchTerm) => {
+        setSearchTerm(searchTerm);
+    };
+    useEffect(() => {
+        const results = movies.filter(movie =>
+            movie.titre.toLowerCase().includes(searchTerm)
+        );
+    
+        setSearchResults(results);
+        console.log(searchResults.map(item => (
+            { item }
+        )))
+    }, [searchTerm]);
+    
+
+
+    return (
+        
+        <div className='AppMovies'>
+
+            <>
+                <Navbar bg="light" variant="light" fixed="top">
+                    <Navbar.Brand  href="#home">Movie Card</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">About</Nav.Link>
+                        <Nav.Link href="#pricing">Contact</Nav.Link>
+                    </Nav>
+                    <Form inline>
+                        <Filtre searchTerm={searchTerm} handleChange={handleChange} />
+                        
+                        <Button variant="primary" class="btn btn-dark"onClick={handleShow}>Add Movie</Button>
+                    </Form>
+                </Navbar>
+
+
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Add New Movie</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        
+                        <MovieAdd handleMovieAdd={handleMovieAdd}></MovieAdd>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+          </Button>
+                   
+                    </Modal.Footer>
+                </Modal>
+            </>
+
+            
+
+            <MovieAdd handleMovieAdd={handleMovieAdd}></MovieAdd>
+            
+
+            <MovieList movies={movies.filter(movie => movie.titre.toLowerCase().includes(searchTerm))} />
+        </div>
+    );
+};
+
+export default AppMovies;
